229d154722d8da24090962611d79a53800755baa
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-27 13:57:31 +0800

    feat: handle pagination for feed route

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,6 +1,8 @@
 class AssetsController < ApplicationController
+  MAX_PAGINATION_LIMIT = 50
+
   def index
-    @assets = Asset.all
+    @assets = Asset.order(taken_at: :desc).limit(result_limit).offset(params[:offset])
 
     render json: { assets: @assets }, status: :ok
   end
@@ -13,5 +15,11 @@ class AssetsController < ApplicationController
     else
       render json: { error: "The asset with id #{params[:id]} does not exist." }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def result_limit
+    [params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i, MAX_PAGINATION_LIMIT].min
   end
 end
