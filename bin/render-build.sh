eab0f08d1cb1e462a5834c7c51f02fe57d5c0b3e
Ahmad Ihsan <ihsanashi@gmail.com>
2022-10-29 18:35:43 +0800

    fix: don't precompile assets in execution file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile # Unneeded as this is an API only application with no views or assets
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:seed
